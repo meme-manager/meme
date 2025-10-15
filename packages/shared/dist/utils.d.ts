ba7c036110455696662ed9efe678ce68ed202f0e
wangding <wangding@shwoody.com>
2025-10-15 18:11:18 +0800

    fix: 完全清理所有应该被忽略的文件,添加验证脚本

--- packages/shared/dist/utils.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function generateId(): string;
-export declare function generateEventId(): string;
-export declare function generateDeviceId(): string;
-export declare function calculateContentHash(content: ArrayBuffer): Promise<string>;
